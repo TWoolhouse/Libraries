e93f336ee95ad465a589fdf7f90f68b26bc2415c
TWoolhouse <thomasrwoolhouse@gmail.com>
2019-09-01 22:11:02 +0100

    moved disptach to overload and added threading dispatching

--- dispatch.py
+++ dispatch.py
@@ -1,34 +1,120 @@
-_funcs = {}
+import threading
+import queue
 
-def dispatch(*types, default=False):
-    def dispatch(func):
-        name = func.__module__+"."+func.__qualname__
-        if name not in _funcs:
-            _funcs[name] = [{}, None]
-        if not default:
-            _funcs[name][0][types] = func
-        else:
-            _funcs[name][1] = func
-        def dispatch(*args, **kwargs):
-            funcs = _funcs[name]
-            for f in funcs[0]:
-                if f != "default":
-                    akws = (*args, *kwargs.values())
-                    if all((isinstance(a, t) if t != None else True for t,a in zip(f, akws))):
-                        return funcs[0][f](*args, **kwargs)
-            if funcs[1]:
-                return funcs[1](*args, **kwargs)
-            raise NotImplementedError()
-        return dispatch
-    return dispatch
+class Dispatcher:
+    def __init__(self, num=1):
+        self.workers = {threading.Thread(target=self.dispatch) for i in range(num)}
+        self.running = threading.Event()
+        self.event = threading.Event()
+        self.queue = queue.Queue()
+        self.active = None
+        self.data = {}
+        self.output = {}
 
-# @dispatch(default=True)
-# def test(name):
-#     print(type(name), name)
-#
-# @dispatch(int)
-# def test(val1):
-#     print("int", type(val1), val1)
-#
-# test(5)
-# test([2])
+        self.new(self.active)
+
+        self.running.set()
+        for w in self.workers:
+            w.start()
+
+    def __call__(self, key):
+        self.queue.put(key)
+        if not self.event.is_set():
+            self.next()
+
+    def __getitem__(self, key):
+        queue = self.output[key]
+        try:
+            for i in range(queue.qsize()):
+                r = queue.get()
+                queue.task_done()
+                yield r
+        except queue.Empty: return
+
+    def __setitem__(self, key, data):
+        self.data[key][0].put(data)
+
+    def new(self, key, func=None, value=None, output=False):
+        self.data[key] = (queue.Queue(), func, value, output)
+        if output:
+            self.output[key] = queue.Queue()
+
+    def next(self):
+        self.event.clear()
+        try:
+            key = self.queue.get_nowait()
+            self.active = key
+            self.queue.task_done()
+            self.event.set()
+        except queue.Empty: pass
+
+    def dispatch(self):
+        while self.running.is_set():
+            self.event.wait()
+            que, func, value, is_output = self.data[self.active]
+            try:
+                if value:
+                    if func:
+                        if is_output:
+                            output = self.output[self.active]
+                            while True:
+                                item = que.get_nowait()
+                                r = func(value, item)
+                                que.task_done()
+                                output.put(r)
+                        else:
+                            while True:
+                                item = que.get_nowait()
+                                func(value, item)
+                                que.task_done()
+                    else:
+                        if is_output:
+                            output = self.output[self.active]
+                            while True:
+                                item = que.get_nowait()
+                                r = item(value)
+                                que.task_done()
+                                output.put(r)
+                        else:
+                            while True:
+                                item = que.get_nowait()
+                                item(value)
+                                que.task_done()
+                else:
+                    if func:
+                        if is_output:
+                            output = self.output[self.active]
+                            while True:
+                                item = que.get_nowait()
+                                r = func(item)
+                                que.task_done()
+                                output.put(r)
+                        else:
+                            while True:
+                                item = que.get_nowait()
+                                func(item)
+                                que.task_done()
+                    else:
+                        if is_output:
+                            output = self.output[self.active]
+                            while True:
+                                item = que.get_nowait()
+                                r = item()
+                                que.task_done()
+                                output.put(r)
+                        else:
+                            while True:
+                                item = que.get_nowait()
+                                item()
+                                que.task_done()
+            except queue.Empty:
+                if not self.event.is_set():
+                    continue
+                self.next()
+
+    def wait(self, key):
+        self.data[key][0].join()
+
+    def stop(self):
+        self.running.clear()
+        self.event.set()
---
+++ overload.py
@@ -0,0 +1,34 @@
+_funcs = {}
+
+def dispatch(*types, default=False):
+    def dispatch(func):
+        name = func.__module__+"."+func.__qualname__
+        if name not in _funcs:
+            _funcs[name] = [{}, None]
+        if not default:
+            _funcs[name][0][types] = func
+        else:
+            _funcs[name][1] = func
+        def dispatch(*args, **kwargs):
+            funcs = _funcs[name]
+            for f in funcs[0]:
+                if f != "default":
+                    akws = (*args, *kwargs.values())
+                    if all((isinstance(a, t) if t != None else True for t,a in zip(f, akws))):
+                        return funcs[0][f](*args, **kwargs)
+            if funcs[1]:
+                return funcs[1](*args, **kwargs)
+            raise NotImplementedError()
+        return dispatch
+    return dispatch
+
+# @dispatch(default=True)
+# def test(name):
+#     print(type(name), name)
+#
+# @dispatch(int)
+# def test(val1):
+#     print("int", type(val1), val1)
+#
+# test(5)
+# test([2])
