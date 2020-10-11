29490172eed9aa1a9b8e93e45eba1ac629f12aff
TWoolhouse <thomasrwoolhouse@gmail.com>
2020-10-11 13:55:55 +0100

    sound private methods

--- sound.py
+++ sound.py
@@ -2,6 +2,7 @@ import sinput
 import subprocess
 import time
 import winsound
+import keys
 
 # cmd = "ffplay -loglevel quiet -nodisp \""+file+"\""
 # os.system(cmd)
@@ -10,7 +11,6 @@ import winsound
 
 _ffplay = "D:/Programs/ffmpeg/bin/ffplay.exe"
 _winsound = True
-_volume = None
 
 try:
     subprocess.run(_ffplay, capture_output=True)
@@ -57,51 +57,61 @@ class Song:
 
 class Volume:
 
-    def __init__(self, vol=0):
-        global _volume
-        if _volume == None:
-            for i in range(50):
-                self._volume_down()
-            _volume = self.constrain(vol) // 2 * 2
-            for i in range(_volume // 2):
-                self._volume_up()
+    __volume = None
 
-    def constrain(self, val, min=0, max=100):
+    def __init__(self, sync: int=0):
+        if sync != False or self.__volume is None:
+            self.sync(sync)
+
+    def __constrain(self, val: int, min: int=0, max: int=100):
         return (min if val < min else (max if val > max else val))
 
-    def _volume_up(self):
-        sinput.key(sinput.VK.VOLUME_UP)
+    def __volume_up(self):
+        sinput.key(keys.VOLUME_UP)
 
-    def _volume_down(self):
-        sinput.key(sinput.VK.VOLUME_DOWN)
+    def __volume_down(self):
+        sinput.key(keys.VOLUME_DOWN)
 
-    def set_volume(self, vol=0, rel=False):
-        global _volume
+    def set(self, vol: int=0, rel: bool=False):
         if rel:
-            vol += _volume
-        vol = self.constrain(vol)
-        diff = vol - _volume
+            vol += self.__volume
+        vol = self.__constrain(vol)
+        diff = vol - self.__volume
         if diff > 0:
             for i in range(abs(diff // 2)):
-                self._volume_up()
+                self.__volume_up()
         elif diff < 0:
             for i in range(abs(diff // 2)):
-                self._volume_down()
-        _volume = vol // 2 * 2
+                self.__volume_down()
+        self.__volume = vol // 2 * 2
         return self.volume()
 
+    def up(self, amt: int=2):
+        self.set(amt, True)
+    def down(self, amt: int=2):
+        return self.up(-amt)
+
     def volume(self):
-        global _volume
-        return _volume
+        return self.__volume
 
-class Media:
+    def sync(self, value: int=0):
+        for i in range(50):
+            self.__volume_down()
+        self.__volume = self.__constrain(value) // 2 * 2
+        for i in range(self.__volume // 2):
+            self.__volume_up()
 
-    def pause(self):
-        sinput.key(sinput.VK.MEDIA_PLAY_PAUSE)
+class media:
+
+    @staticmethod
+    def pause():
+        sinput.key(keys.MEDIA_PLAY_PAUSE)
     play = pause
 
-    def next(self):
-        sinput.key(sinput.VK.MEDIA_NEXT_TRACK)
+    @staticmethod
+    def next():
+        sinput.key(keys.MEDIA_NEXT_TRACK)
 
-    def prev(self):
-        sinput.key(sinput.VK.MEDIA_PREV_TRACK)
+    @staticmethod
+    def prev():
+        sinput.key(keys.MEDIA_PREV_TRACK)
