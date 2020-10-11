aa7fd9603dae40cb7dd413697eedaeee36b06658
TWoolhouse <thomasrwoolhouse@gmail.com>
2020-10-11 13:59:39 +0100

    keep connection alive + SSL

--- website/buffer.py
+++ website/buffer.py
@@ -3,8 +3,12 @@ from interface import Interface
 import re
 import json
 from . import error
+from path import PATH
+from caching import cache
 
-__all__ = ["Buffer", "File", "Python", "wrap", "wrap"]
+import debug
+
+__all__ = ["Buffer", "File", "Python", "wrap", "clear"]
 
 def wrap(func):
     def wrapper(*args, **kwargs):
@@ -16,63 +20,103 @@ def wrap(func):
         return inner
     return wrapper
 
+# def clear():
+#     File.open.
+
 class Buffer:
 
-    def __init__(self):
-        pass
+    cache_disable = False
+
+    def __init__(self, cache_disable: bool=None):
+        if cache_disable is not None:
+            self.cache_disable = cache_disable
+
+    async def open(self) -> bytes:
+        raise error.BufferRead(self)
+    async def data(self) -> bytes:
+        return await self.open()
 
     async def compile(self) -> bytes:
-        return b"BUFFER"
+        return await self.open()
 
     def __str__(self) -> str:
         return f"Base Buffer"
 
 class File(Buffer):
 
-    def __init__(self, file: str):
+    def __init__(self, file: str, cache_disable: bool=None):
+        super().__init__(cache_disable)
         self.file = file
 
-    async def compile(self) -> bytes:
+    @staticmethod
+    @cache
+    async def open(filename) -> bytes:
         try:
-            async with Interface.open(self.file, "rb") as file:
+            async with Interface.open(filename, "rb") as file:
                 return file.read()
         except FileNotFoundError:
-            raise error.BufferRead(self) from None
+            raise error.BufferRead(filename) from None
+
+    async def compile(self) -> bytes:
+        return await self.open(self.file, override=self.cache_disable)
 
     def __str__(self) -> str:
         return f"File<{self.file}>"
 
-
 class Python(File):
 
-    __re = re.compile("(?:<\\?python)((?:(?!\\?>)(?:\\r?\\n|.))*)(?:\\?>)")
+    __re = re.compile(r"(?:<py>)((?:(?!</py>)(?:\r?\n|.))*)(?:</py>)")
 
-    def __init__(self, file: str, request: "Request"):
-        super().__init__(file)
+    def __init__(self, file: str, request: "Request", cache_disable: bool=None):
+        super().__init__(file, cache_disable)
         self.__request = request
 
-    async def compile(self) -> bytes:
+    def __await__(self):
+        return self.compile().__await__()
+
+    @staticmethod
+    @cache
+    async def open(filename) -> bytes:
         try:
-            async with Interface.open(self.file, "r", encoding="utf-8") as file:
-                data = file.read()
+            async with Interface.open(filename, "r", encoding="utf-8") as file:
+                return file.read()
         except FileNotFoundError:
-            raise error.BufferRead(self) from None
+            raise error.BufferRead(filename) from None
+
+    @debug.catch
+    async def compile(self, request: object=None) -> bytes:
+        data = await self.open(self.file, override=self.cache_disable)
         namespace = {
-            "request": self.__request,
+            "path": PATH,
+            "request": request or self.__request,
             "value": self.__wrap,
+            "buffer": self.__insert_buffer,
         }
         match = self.__re.search(data)
+
+        output = b""
         while match:
+            try:
+                new = await eval(match[1].strip(), namespace)()
+            except Exception:
+                print(match[1].strip())
+                raise
+            if not isinstance(new, bytes):
+                new = str(new).encode("utf8")
             start, end = match.span()
-            new = str(await eval(match[1].strip(), namespace)())
-            data = data[:start] + new + data[end:]
-            match = self.__re.search(data, start+len(new))
-        return data.encode("utf-8")
+            output += data[:start].encode("utf8") + new
+            data = data[end:]
+            match = self.__re.search(data)
+        return output + data.encode("utf8")
 
     def __wrap(self, value):
         async def wrapper():
+            if asyncio.iscoroutine(value):
+                return await value
             return value
         return wrapper
+    def __insert_buffer(self, path, type="py"): # Implement Types
+        return Python(path, self.__request).compile
 
     def __str__(self) -> str:
         return f"Python<File<{self.file}> Request<{self.__request}>>"
@@ -80,6 +124,7 @@ class Python(File):
 class Json(Buffer):
 
     def __init__(self, data):
+        super().__init__()
         self.data = data
 
     async def compile(self) -> bytes:
@@ -87,3 +132,4 @@ class Json(Buffer):
 
     def __str__(self) -> str:
         return f"JSON<{self.data}>"
+
--- website/server.py
+++ website/server.py
@@ -1,10 +1,16 @@
+import debug
 import asyncio
 from interface import Interface
+import socket
+import ssl
 import urllib.parse
 from http import HTTPStatus as Status
 from http.cookies import SimpleCookie as Cookie
 from .buffer import Buffer
 from . import error
+from ._ssl import context as ssl_context
+import functools
+from typing import Type
 
 import traceback
 
@@ -36,9 +42,10 @@ class Sessions:
         self.__container.__delitem__(key)
 
 class Header:
-    def __init__(self, name: str, *values: str):
+    def __init__(self, name: str, *values: str, split="; "):
         self.name = name.title().replace(" ", "-")
         self.values = list(values)
+        self.split = split
 
     @property
     def value(self):
@@ -51,7 +58,7 @@ class Header:
         return f"{self.name}<{', '.join(map(str, self.values))}>"
 
     def _format(self) -> str:
-        return f"{self.name.title().replace(' ', '-')}: {'; '.join(map(str, self.values))}"
+        return f"{self.name.title().replace(' ', '-')}: {self.split.join(map(str, self.values))}"
 
 class HeaderContainer:
 
@@ -64,6 +71,7 @@ class HeaderContainer:
     def __setitem__(self, key, value: Header):
         assert isinstance(value, Header), f"'{self.__class__.__name__}' Can only Store '{Header.__name__}'"
         self.__container[key.title().replace(" ", "-")] = value
+        return value
 
     def __delitem__(self, key):
         return self.__container.__delitem__(key.title().replace(" ", "-"))
@@ -71,8 +79,11 @@ class HeaderContainer:
     def __iter__(self):
         yield from self.__container.items()
 
-    def get(self, key) -> Header:
-        return self.__getitem__(key)
+    def get(self, key, default=None) -> Header:
+        try:
+            return self.__getitem__(key)
+        except KeyError:
+            return default
     def set(self, key, value) -> Header:
         return self.__setitem__(key, value)
     def pop(self, key):
@@ -84,6 +95,9 @@ class HeaderContainer:
 
     def _format(self):
         return "\r\n".join(head._format() for head in self.__container.values())
+
+    def __contains__(self, key: str) -> bool:
+        return key.title().replace(" ", "-") in self.__container
 
 class BufferContainer:
 
@@ -106,7 +120,7 @@ class BufferContainer:
             elif isinstance(buffer, bytes):
                 pass
             elif isinstance(buffer, str):
-                buffer = buff.encode("utf-8")
+                buffer = buffer.encode("utf-8")
             else:
                 continue
             final += buffer
@@ -115,22 +129,39 @@ class BufferContainer:
 
 class Client:
 
-    def __init__(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter, addr: str, port: int, command: str, query: dict, request: {str, Header}, cookies: Cookie):
+    def __init__(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter, server, server_addr: str, peer_addr: str, peer_port: int, ssl: bool, command: str, path: str, query: dict, request: HeaderContainer, cookies: Cookie):
         self.__reader, self._writer = reader, writer
-        self.host, self.port = addr, port
-        self.command, self.query, self.request, self.cookie = command, query, request, cookies
+        self.peer, self.port = peer_addr, peer_port
+        self.hostname = server_addr
+        self.server = server
+        self.ssl = ssl
+        self.command, self.path, self.query, self.request, self.cookie = command, path, query, request, cookies
         self.status = Status.OK
         self.header = HeaderContainer()
         self.buffer = BufferContainer()
         self.session = Session(None)
+        self.close = bool(self.request.get("Connection", "").value.lower() != "keep-alive")
+
+class DefaultRequest:
+    class __base:
+        def __init__(self, client: Client, request: list, seg: int, *args, **kwargs):
+            raise RuntimeError
+    class ssl(__base):
+        pass
+    class redirect(__base):
+        pass
 
 class Request:
 
+    default = DefaultRequest
     def __init__(self, client: Client, request: list, seg: int, *args, **kwargs):
         self.client = client
         self.request = request
         self.seg = seg
         self.init(*args, **kwargs)
+
+    def __await__(self):
+        return self.handle().__await__()
 
     def init(self, *args, **kwargs):
         pass
@@ -138,114 +169,184 @@ class Request:
     async def handle(self) -> "Request":
         pass
 
+    def secure(func):
+        @functools.wraps(func)
+        async def enforce_secure(self):
+            if self.client.ssl:
+                return await func(self)
+            return self.default.ssl(self.client, self.request, self.seg)
+        return enforce_secure
+
+    def redirect(path=None, query=None, hostname=None, port=None, scheme="http"):
+        class redirect(DefaultRequest.redirect):
+            def init(self, *args, **kwargs):
+                super().init(path, query, hostname, port, scheme)
+        return redirect
+
+class DefaultRequest:
+    class redirect(Request):
+        def init(self, path=None, query=None, hostname=None, port=None, scheme=None):
+            self.path = path or "/".join(self.request)
+            self.query = query
+            self.hostname = hostname or self.client.hostname
+            self.port = port or (self.client.server.https if self.client.ssl else self.client.server.http)
+            self.scheme = scheme or ("https" if self.client.ssl else "http")
+        async def handle(self):
+            self.client.status = self.client.status.PERMANENT_REDIRECT
+            self.client.header << Header("Location", f"{self.scheme}://{self.hostname}:{self.port}/{self.path}{f'?{self.query}' if self.query else ''}")
+    class ssl(redirect):
+        def init(self, *args, **kwargs):
+            super().init("/".join(self.request), None, self.client.hostname, self.client.server.https, "https")
+            # self.client.header << Header("Location", f"https://{self.client.hostname}:{self.client.server.https}/{'/'.join(self.request)}")
+
+Request.default = DefaultRequest
+
 class Tree:
-    def __init__(self, blank__: Request=None, end__=error.TreeTraversal, default__=error.TreeTraversal, **dirs):
+    def __init__(self, blank__: Request=error.TreeTraversal, end__=error.TreeTraversal, default__=error.TreeTraversal, **dirs):
         self.__tree = {
             "": blank__,
         }
         for k,v in dirs.items():
             if isinstance(v, dict):
                 v = Tree(**v)
-            if isinstance(v, (Tree, Request, Exception)) or callable(v):
-                tree = v
-            self.__tree[k] = tree
+            # if isinstance(v, (Tree, Request, Exception)) or callable(v):
+            #     tree = v
+            self.__tree[k] = v
         self.end = end__
         self.default = default__
 
-    def traverse(self, request: list, segment: int, client: Client) -> Request:
+    @debug.log
+    def traverse(self, request: list, segment: int, client: Client, *args, **kwargs) -> Request:
         try:
             req = request[segment]
             segment += 1
             req = self.__tree[req]
         except IndexError as e:
+            segment += 1
             if issubclass(self.end, Exception):
                 raise self.end(self, request, segment) from None
-            return self.end(client, request, segment)
+            req = self.end
         except KeyError as e:
             if issubclass(self.default, Exception):
                 raise self.default(self, request, segment, req) from None
-            return self.default(client, request, segment)
+            req = self.default
         if isinstance(req, Tree):
-            return req.traverse(request, segment, client)
-        elif issubclass(req, Request):
-            return req(client, request, segment)
-        elif issubclass(req, Exception):
-            raise req()
+            return req.traverse(request, segment, client, *args, **kwargs)
+        elif isinstance(req, type):
+            if issubclass(req, Request):
+                return req(client, request, segment, *args, **kwargs)
+            elif issubclass(req, Exception):
+                raise req(self, request, segment)
+        elif isinstance(req, Buffer):
+            client.buffer << req
+            return req
+        elif callable(req):
+            return req(*args, **kwargs)
         else:
-            return req()
+            return req
+
+    def insert(self, key: str, item) -> "Tree":
+        if isinstance(item, dict):
+            item = Tree(**item)
+        self.__tree[key] = item
 
 class Server:
 
-    def __init__(self, request: Request, port: int=80, host: str=None, client: type(Client)=Client):
+    class Data:
+        def __init__(self, http: int, https: int):
+            self.http, self.https = http, https
+
+    def __init__(self, request: Type[Request], port: int=80, host: str="", client: Type[Client]=Client, ssl: int=None):
         self.request = request
         self.client = client
         self.host, self.port = host, port
+        self.ssl = ssl
+        self.data = Server.Data(self.port, self.ssl)
 
     async def serve(self):
-        self.__server = await asyncio.start_server(self.__create_client, self.host, self.port)
-
-        async with self.__server:
+        debug.print(f"Opening Server: {self.port}{' && {}'.format(self.ssl) if self.ssl else ''}")
+        self.__server = await asyncio.start_server(self.__create_client, self.host, self.port, backlog=500, start_serving=False)
+        self.__server_ssl = await asyncio.start_server(lambda r, w: self.__create_client(r, w, True), self.host, self.ssl, backlog=500, ssl=ssl_context, start_serving=False) if self.ssl is not None else self.__server
+        async with self.__server, self.__server_ssl:
+            if self.__server_ssl is not self.__server:
+                await self.__server_ssl.start_serving()
             await self.__server.serve_forever()
 
-    async def __create_client(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
-        addr = writer.get_extra_info('peername')
-        try:
-            # Get First Line of the Request
-            request = (await reader.readline()).decode().strip()
-            # print("Request:", request)
-
+    async def __create_client(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter, ssl=False):
+        TIMEOUT = 300
+        addr = writer.get_extra_info("peername")
+        debug.print("Connection:", *addr)
+        alive = True
+        while alive:
+            alive = False
             try:
-                command, url, version = request.split()
-            except ValueError:    return
-            url = urllib.parse.urlparse(url)
-            path = url.path or "/"
-            query = dict(map(urllib.parse.unquote, q.split("=", 1)) for q in url.query.split("&")) if url.query else {}
+                # Get First Line of the Request
+                try:
+                    request = await asyncio.wait_for(reader.readline(), TIMEOUT)
+                except asyncio.TimeoutError:
+                    return writer.close()
+                # print("Request:", *addr, request)
 
-            # Parse Headers
-            headers = {}
-            while (line := (await reader.readline()).decode().strip()):
-                name, values = map(str.strip, line.split(":", 1))
-                headers[name.lower()] = Header(name, values)
+                try:
+                    command, url, version = request.decode().strip().split()
+                except ValueError:    return False
+                url = urllib.parse.urlparse(url)
+                path = url.path or "/"
+                query = dict(map(lambda x: urllib.parse.unquote(x).replace("+", " "), q.split("=", 1)) for q in url.query.split("&")) if url.query else {}
 
-            # Read body
-            if "content-length" in headers:
-                body = (await reader.read(int(headers["content-length"].value)+1)).decode().strip()
-                if body:
-                    query.update(map(urllib.parse.unquote, q.split("=", 1)) for q in body.split("&"))
+                # Parse Headers
+                headers = HeaderContainer()
+                while (line := (await reader.readline()).decode().strip()):
+                    name, values = map(str.strip, line.split(":", 1))
+                    headers[name] = Header(name, values)
 
-            # Parse Cookies
-            cookies = Cookie()
-            if "cookie" in headers:
-                for cookie in headers["cookie"].value.split(";"):
-                    name, value = cookie.strip().split("=")
-                    cookies[name] = value
+                # Read body
+                if "content-length" in headers:
+                    body = (await reader.read(int(headers["content-length"].value)+1)).decode().strip()
+                    if body:
+                        query.update(map(urllib.parse.unquote, q.split("=", 1)) for q in body.split("&"))
 
-            # print(command, url.hostname, path)
-            # print(query)
-            # print(headers)
-            # print(cookies)
-            client = self.client(reader, writer, *addr, command, query, path, cookies)
-            request = self.request(client, path.split("/")[1:], 0)
+                # Parse Cookies
+                cookies = Cookie()
+                if "cookie" in headers:
+                    for cookie in headers["cookie"].value.split(";"):
+                        name, value = cookie.strip().split("=")
+                        cookies[name] = value
 
-            # print("Request Handler")
-            while isinstance(request, Request):
-                # print(f"Proc Req: {type(request).__qualname__} {request}")
-                request = await request.handle()
-            # print("Request Handled")
+                # print(command, url.hostname, path)
+                # print(query)
+                # print(headers)
+                # print(cookies)
+                client = self.client(reader, writer, self.data, headers["host"].value.split(":", 1)[0], *addr, ssl, command, path, query, headers, cookies)
+                request = self.request(client, path.split("/")[1:], 0)
 
-            # Compile Client Buffer
-            cbuffer = await client.buffer._compile(client)
+                # print("Request Handler")
+                while isinstance(request, Request):
+                    # print(f"Proc Req: {type(request).__qualname__} {request}")
+                    request = await request.handle()
+                # print("Request Handled")
 
-            buffer = "\r\n".join(filter(None, (
-                f"HTTP/1.1 {client.status.value} {client.status.phrase}",
-                client.header._format(),
-                client.cookie.output(),
-            ))).strip() + "\r\n\r\n"
+                # Compile Client Buffer
+                cbuffer = (await client.buffer._compile(client)).strip()
 
-            writer.write(buffer.encode())
-            writer.write(cbuffer.strip())
-        except Exception as e:
-            print("Connection:", "".join(traceback.format_exception(e, e, e.__traceback__)))
-        finally:
-            await writer.drain()
-            writer.close()
+                client.header << Header("Content-Length", len(cbuffer))
+                if "Connection" in headers and headers["Connection"].value == "keep-alive":
+                    client.header << Header("Connection", "keep-alive")
+                    client.header << Header("Keep-Alive", f"timeout={TIMEOUT}", split=", ")
+                    alive = True
+                buffer = "\r\n".join(filter(None, (
+                    f"HTTP/1.1 {client.status.value} {client.status.phrase}",
+                    client.header._format(),
+                    client.cookie.output(),
+                ))).strip() + "\r\n\r\n"
+
+                writer.write(buffer.encode())
+                writer.write(cbuffer)
+            except Exception as e:
+                print(f"Connection: {addr[0]}:{addr[1]}", "".join(traceback.format_exception(e, e, e.__traceback__)))
+            finally:
+                try:
+                    await writer.drain()
+                except ConnectionError:    pass
+                if not alive:
+                    writer.close()
