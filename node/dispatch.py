42eb231d5de41aa8461a263eec21a7ef095a4ae6
TWoolhouse <thomasrwoolhouse@gmail.com>
2021-01-05 16:39:46 +0000

    syntax

--- node/dispatch.py
+++ node/dispatch.py
@@ -1,10 +1,10 @@
 from .data import Data
+import time
 
 async def echo(node: 'DataInterface', data: Data) -> bool:
     await node.send(data)
     return True
 
-import time
 
 async def ping(node: 'DataInterface', data: Data) -> bool:
     await node.send(time.time() - float(data.data), "ping")
