a2f592c623dc1086012ad90be1b519fbaf55a3b4
TWoolhouse <thomasrwoolhouse@gmail.com>
2021-01-05 16:27:33 +0000

    randstate typing

--- randstate.py
+++ randstate.py
@@ -1,26 +1,30 @@
 import random
+import functools
 
 __random = random.Random()
 
-def load(rand, state=None):
+def load(rand: 'Random', state=None):
     if not isinstance(rand, Random):
         raise ValueError("Must be of type Random")
     def load(func):
+        @functools.wraps(func)
         def load(*args, **kwargs):
             rand.load(state)
             return func(*args, **kwargs)
         return load
     return load
 
-def save(rand, after=True):
+def save(rand: 'Random', after=True):
     if not isinstance(rand, Random):
         raise ValueError("Must be of type Random")
     def save(func):
         if after:
+            @functools.wraps(func)
             def save(*args, **kwargs):
                 return func(*args, **kwargs)
                 rand.save()
         else:
+            @functools.wraps(func)
             def save(*args, **kwargs):
                 rand.save()
                 return func(*args, **kwargs)
@@ -33,15 +37,15 @@ class Random(random.Random):
         super().__init__(seed)
         self.set()
 
-    def load(self, state=None):
+    def load(self, state=None) -> 'Random':
         self.setstate(self.rs_state if state == None else state)
         return self
 
-    def save(self, state=None):
+    def save(self, state=None) -> 'Random':
         self.rs_state = self.getstate() if state == None else state
         return self
 
-    def set(self, state=None):
+    def set(self, state=None) -> 'Random':
         self.save(state)
         self.load()
         return self
