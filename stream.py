74f406c41d4deee6c928f03804e7cc91cc6627c9
TWoolhouse <thomasrwoolhouse@gmail.com>
2021-02-07 14:07:26 +0000

    In-Memory Stream numpy support

--- stream.py
+++ stream.py
@@ -1,20 +1,21 @@
 import io
 import collections
+import numpy as np
 
 class Stream(io.BufferedIOBase):
     def __init__(self, blocksize=4096):
         super().__init__()
-        self.__closed = False
+        self._closed = False
         self._blocksize = blocksize
-        self.__index = 0
-        self.__data = collections.deque()
+        self._index = 0
+        self._data = collections.deque()
 
     def close(self):
-        if not self.__closed:
-            self.__closed = True
+        if not self._closed:
+            self._closed = True
     @property
     def closed(self) -> bool:
-        return self.__closed
+        return self._closed
 
     def detach(self):
         raise io.UnsupportedOperation()
@@ -25,34 +26,36 @@ class Stream(io.BufferedIOBase):
     def isatty(self):
         return False
 
-    def read(self, size=-1) -> bytes:
-        if size == 0:
-            return b""
-
+    def _read_blocks(self, size: int) -> list:
         if size < 0:
-            size = self._blocksize * len(self.__data)
+            size = self._blocksize * len(self._data)
 
         diff = size
         blocks = []
 
         while diff > 0:
-            if not self.__data:
+            if not self._data:
                 break
 
-            block = self.__data[0][self.__index:]
+            block = self._data[0][self._index:]
 
             if len(block) > diff:
                 block = block[:diff]
 
-            self.__index += len(block)
-            if self.__index == len(self.__data[0]):
-                self.__index = 0
-                self.__data.popleft()
+            self._index += len(block)
+            if self._index == len(self._data[0]):
+                self._index = 0
+                self._data.popleft()
 
             diff -= len(block)
             blocks.append(block)
+        return blocks
 
-        return b"".join(blocks)
+    def read(self, size=-1) -> bytes:
+        if size == 0:
+            return b""
+
+        return b"".join(self._read_blocks(size))
 
     read1 = read
     def readable(self) -> bool:
@@ -73,8 +76,8 @@ class Stream(io.BufferedIOBase):
     def truncate(self, size: int=0) -> int:
         if size != 0:
             raise io.UnsupportedOperation("Can only Truncate to 0 bytes")
-        self.__data.clear()
-        self.__index = 0
+        self._data.clear()
+        self._index = 0
         return 0
 
     def writable(self) -> bool:
@@ -86,4 +89,28 @@ class Stream(io.BufferedIOBase):
         while index < size:
             block = bytes(buffer[index:index+self._blocksize])
             index += len(block)
-            self.__data.append(block)
+            self._data.append(block)
+
+class StreamArray(Stream):
+
+    def __init__(self, blocksize=4096, shape=(1,), dtype=None):
+        super().__init__(blocksize)
+        self._shape = shape
+        self._dtype = dtype
+
+    def read(self, size=-1) -> np.ndarray:
+        if size == 0:
+            return np.empty((0, self._shape[1]), dtype=self._dtype)
+
+        blocks = self._read_blocks(size)
+        if blocks:
+            return np.concatenate(blocks)
+        return np.zeros((0, self._shape[1]), dtype=self._dtype)
+
+    def write(self, buffer: np.ndarray):
+        index = 0
+        size = len(buffer)
+        while index < size:
+            block = buffer[index:index+self._blocksize]
+            index += len(block)
+            self._data.append(block)
