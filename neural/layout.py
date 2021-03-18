26d754ee218ca4c70e24ba26a4d1f89756166ec3
TWoolhouse <thomasrwoolhouse@gmail.com>
2021-03-18 21:12:42 +0000

    layout sate saving and support for different neuron types

--- neural/layout.py
+++ neural/layout.py
@@ -1,37 +1,34 @@
-from neural.neuron import Neuron as _Neuron
+from .neuron import N as Neuron
 
 class Layout:
+    """A specification for the network.
+    It tells the netowkr how to link all the neurons togeather and what type they should be."""
 
-    def __init__(self, data: tuple, input: tuple, output: tuple):
-        self.__input, self.__output = input, output
-        self.__data = data
+    def __init__(self, input: tuple[int], output: tuple[int], neurons: tuple[tuple[int, tuple[int], type[Neuron]]]):
+        self.inputs, self.outputs = input, output # Index of input and output neurons
+        self.neurons = neurons # Array of neurons and links
 
-    def compile(self) -> ((_Neuron,), (_Neuron,), [_Neuron,]):
-        neurons = [_Neuron(i) for i in range(len(self.__data))]
-        for neuron, connections in zip(neurons, self.__data):
-            for conn in connections:
-                neuron._connections[conn] = [neurons[conn], 0]
-        return tuple(neurons[i] for i in self.__input), tuple(neurons[i] for i in self.__output), neurons
+    def __getstate__(self) -> tuple[tuple[int], tuple[int], tuple[tuple[int, tuple[int], type[Neuron]]]]:
+        return self.inputs, self.outputs, self.neurons
+    def __setstate__(self, data: tuple[tuple[int], tuple[int], tuple[tuple[int, tuple[int], type[Neuron]]]]):
+        self.inputs, self.outputs, self.neurons = data
 
-    def __getstate__(self) -> tuple:
-        return self.__input, self.__output, self.__data
-    def __setstate__(self, data: tuple):
-        self.__input, self.__output, self.__data = data
-
-def FeedForward(inputs: int, outputs: int, *layers: int) -> Layout:
-    size = sum(layers)
+def FeedForward(inputs: tuple[int, type[Neuron]], outputs: tuple[int, type[Neuron]], *layers: tuple[int, type[Neuron]]) -> Layout:
+    """Create the spec for a feed forward network.
+    Where all nodes are directly connected to the next column of rows."""
+    size = sum(i[0] for i in layers)
     grid = [inputs, *layers, outputs]
-    inputs, outputs = tuple(range(inputs)), tuple(range(inputs+size, inputs+size+outputs))
+    inputs, outputs = tuple(range(inputs[0])), tuple(range(inputs[0]+size, inputs[0]+size+outputs[0]))
 
-    layout = [[] for i in range(sum(grid))]
+    layout = [[i, [], v[1]] for v in grid for i in range(v[0])]
     count = len(layout)
     for column in range(len(grid)-1, 0, -1):
-        for row in range(grid[column]-1, -1, -1):
+        for row in range(grid[column][0]-1, -1, -1):
             count -= 1
             neuron = layout[count]
-            sub = grid[column-1]
+            neuron[0] = count
+            sub = grid[column-1][0]
             start = count - sub - row
-            for child in range(sub):
-                neuron.append(start + child)
-    return Layout(layout, inputs, outputs)
-
+            for child in range(start, start+sub):
+                neuron[1].append(child)
+    return Layout(inputs, outputs, layout)
