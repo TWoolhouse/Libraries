631212a3e1fda00ac366483503de36afbe090b73
TWoolhouse <thomasrwoolhouse@gmail.com>
2021-03-18 21:13:49 +0000

    Cleaner network interface, supporting new layout system

--- neural/network.py
+++ neural/network.py
@@ -1,34 +1,47 @@
-from neural.layout import Layout
+from .layout import Layout
+from .neuron import N as Neuron
+from typing import Callable, T
+import pickle
 
 class Network:
 
     def __init__(self, layout: Layout):
         self.layout = layout
-        self.__inputs, self.__outputs, self._neurons = self.layout.compile()
+        self._generate()
 
-    def input(self, *values: float) -> [float,]:
+    def feed(self, *values: float) -> tuple[float]:
+        """Send data into the network and return the output values"""
         self.clear()
-        for n, v in zip(self.__inputs, values):
+        for n,v in zip(self._inputs, values):
             n.set(v)
-        return self.output()
+        return [n.value() for n in self._outputs]
 
-    def output(self) -> [float,]:
-        return [output.value() for output in self.__outputs]
+    def result(self, output: tuple[float], *funcs: Callable[..., T]) -> T:
+        """Call the function that is in the same index as the highest output"""
+        return max(zip(output, funcs))()
 
     def clear(self):
+        for n in self._neurons:
+            n.clear()
+
+    def copy(self) -> 'Network':
+        return pickle.loads(pickle.dumps(self)) # TODO: This is slow
+
+    def __getstate__(self) -> tuple[Layout, tuple[Neuron]]:
+        return self.layout, self._neurons
+    def __setstate__(self, data: tuple[Layout, tuple[Neuron]]):
+        self.layout, self._neurons = data
+        self._inputs = [self._neurons[i] for i in self.layout.inputs]
+        self._outputs = [self._neurons[i] for i in self.layout.outputs]
+
         for neuron in self._neurons:
-            neuron.set(None)
+            for con in [n for n in neuron._connections if isinstance(n, int)]:
+                neuron._connections[self._neurons[con]] = neuron._connections.pop(con)
 
-    def __getstate__(self) -> tuple:
-        return self.layout, self._neurons, tuple(i.id for i in self.__inputs), tuple(i.id for i in self.__outputs)
-    def __setstate__(self, data: tuple):
-        l, n, i, o = data
-        self.layout, self._neurons, self.__inputs, self.__outputs = l, n, tuple(n[v] for v in i), tuple(n[v] for v in o)
+    def _generate(self):
+        self._neurons: tuple[Neuron] = [n[2](n[0]) for n in self.layout.neurons]
+        self._inputs: tuple[Neuron] = [self._neurons[i] for i in self.layout.inputs]
+        self._outputs: tuple[Neuron] = [self._neurons[i] for i in self.layout.outputs]
 
-    def copy(self) -> "Network":
-        network = Network(self.layout)
-        for original, new in zip(self._neurons, network._neurons):
-            id, bias, connections = original.__getstate__()
-            connections = {key: [network._neurons[key], connections[key][1]] for key in connections}
-            new.__setstate__((id, bias, connections))
-        return network
+        for neuron, data in zip(self._neurons, self.layout.neurons):
+            neuron._connections |= {self._neurons[i]: 1.0 for i in data[1]}
