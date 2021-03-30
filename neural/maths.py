324afbeeb401ae06430580ffa32ef19d60f98307
TWoolhouse <thomasrwoolhouse@gmail.com>
2021-03-30 10:12:53 +0100

    sigmoid will not fail on really large exps

--- neural/maths.py
+++ neural/maths.py
@@ -1,7 +1,10 @@
 import math
 
 def sigmoid(x: float) -> float:
-    return 1.0 / (1.0 + math.exp(-x))
+    try:
+        return 1.0 / (1.0 + math.exp(-x))
+    except:
+        return 0
 
 def constrain(n, start, stop):
     return (n - start) / (stop - start)
